5016337ece88e9fa39887c642f6601b318fa640d
Ted Keim <tedkeim@yahoo.com>
2021-04-24 15:18:09 -0400

    added javascript to game.js, created question layout

--- Develop/game.js
+++ Develop/game.js
@@ -1,5 +1,94 @@
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
-const question = document.querySelector('#question');
-const question = document.querySelector('#question');
-const question = document.querySelector('#question');
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    },
+    {
+        question: 'Question 1?',
+        choice1: 'choice a',
+        choice2: 'choice b',
+        choice3: 'choice c',
+        choice4: 'choice d',
+        answer: 2,
+    }
+]
